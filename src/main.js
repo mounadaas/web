c63c515b4ecb846ec2ab467a0a932384cc6509c3
mounadaas <mounadaas19@gmail.com>
2023-07-02 13:33:38 +0100

    first commit

---
+++ src/main.js
@@ -0,0 +1,6 @@
+let mobileMenu = document.querySelector(".mobile-menu");
+let btnMobile = document.querySelector(".mobil-btn");
+
+btnMobile.addEventListener('click',()=>{
+    mobileMenu.classList.toggle("hidden")
+})
